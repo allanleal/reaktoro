a51777b8c1daa1b82b4e86ad33bb3ece9ea641a4
Allan Leal <allan.leal@erdw.ethz.ch>
2019-09-03 07:39:35 +0200

    Fix missing propagation of CXX17 requirement to dependent codes (#109)
    * Move CXX17 compilation feature to Reaktoro target.
    
    This is a more cmake modern way of doing things (by assiging properties to the targets).
    
    * Fix cmake demo
    
    * Fix division by zero in ChemicalProperties::update
    
    * Ensure pybind11 really used c++17
    
    * Add fPIC back

--- cmake/ReaktoroFindDeps.cmake
+++ cmake/ReaktoroFindDeps.cmake
@@ -3,13 +3,8 @@ find_package(Boost REQUIRED)
 
 # Find pybind11 library (if needed) https://github.com/pybind/pybind11
 if(REAKTORO_BUILD_PYTHON)
-
-    if(NOT PYBIND11_PYTHON_VERSION AND NOT PYTHON_EXECUTABLE)
-        find_package(PythonInterp REQUIRED) # PYTHON_EXECUTABLE gets set here
-    endif()
-
-    find_package(pybind11 REQUIRED HINTS ${PROJECT_SOURCE_DIR}/thirdparty/pybind11)
-
+    set(PYBIND11_CPP_STANDARD -std=c++17)  # Ensure pybind11 really uses c++17
+    find_package(pybind11 REQUIRED)
     if(NOT pybind11_FOUND)
         message(WARNING "Could not find pybind11 - the Python module `reaktoro` will not be built.")
         set(REAKTORO_BUILD_PYTHON OFF)
