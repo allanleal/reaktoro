7170abfca756c3576c2ebf192f0bb795e9d00299
Allan Leal <allanmulin@gmail.com>
2020-04-06 15:45:33 +0200

    Adapted activity models for fluid phases to the use of real

--- Reaktoro/Thermodynamics/Models/GaseousChemicalModelIdeal.cpp
+++ Reaktoro/Thermodynamics/Models/GaseousChemicalModelIdeal.cpp
@@ -18,36 +18,24 @@
 #include "GaseousChemicalModelIdeal.hpp"
 
 // Reaktoro includes
-#include <Reaktoro/Common/Constants.hpp>
-#include <Reaktoro/Thermodynamics/Mixtures/GaseousMixture.hpp>
+#include <Reaktoro/Thermodynamics/Mixtures/GeneralMixture.hpp>
 
 namespace Reaktoro {
 
-auto gaseousChemicalModelIdeal(const GaseousMixture& mixture) -> PhaseChemicalModel
+using std::log;
+
+auto fluidChemicalModelIdeal(const GeneralMixture& mixture)-> ActivityModelFn
 {
-    // The state of the gaseous mixture
-    GaseousMixtureState state;
+    MixtureState state;
 
-    // Define the chemical model function of the gaseous phase
-    PhaseChemicalModel model = [=](PhaseChemicalModelResult& res, real T, real P, VectorXrConstRef n) mutable
+    ActivityModelFn model = [=](ActivityProps& res, real T, real P, VectorXrConstRef n) mutable
     {
-        // Evaluate the state of the gaseous mixture
         state = mixture.state(T, P, n);
-
-        // Calculate pressure in bar
-        const real Pbar = 1e-5 * P;
-
-        // The ln of pressure in units of bar
-        const real ln_Pbar = log(Pbar);
-
-        // The result of the ideal model
-        res.ln_activities = log(state.x) + ln_Pbar;
+        const auto Pbar = 1e-5 * P;
+        res.ln_activities = state.x.log() + log(Pbar);
     };
 
     return model;
 }
 
 } // namespace Reaktoro
-
-
-
