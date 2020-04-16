416db63f37013eb46999530d7838ec37b6905a4b
Allan Leal <allan.leal@erdw.ethz.ch>
2020-04-16 16:10:28 +0200

    Commented out several files for now. They will be revised one by one. This is needed to ensure full compilation and test of new code at the moment.

--- Reaktoro/Equilibrium/EquilibriumSolver.hpp
+++ Reaktoro/Equilibrium/EquilibriumSolver.hpp
@@ -1,119 +1,120 @@
-// Reaktoro is a unified framework for modeling chemically reactive systems.
-//
-// Copyright (C) 2014-2018 Allan Leal
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this library. If not, see <http://www.gnu.org/licenses/>.
+// // This file is part of Reaktoro (https://reaktoro.org).
+// //
+// // Reaktoro is free software; you can redistribute it and/or
+// // modify it under the terms of the GNU Lesser General Public
+// // License as published by the Free Software Foundation; either
+// // version 2.1 of the License, or (at your option) any later version.
+// //
+// // Reaktoro is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// // Lesser General Public License for more details.
+// //
+// // You should have received a copy of the GNU Lesser General Public License
+// // along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-#pragma once
+// #pragma once
 
-// C++ includes
-#include <memory>
+// // C++ includes
+// #include <memory>
 
-// Reaktoro includes
-#include <Reaktoro/Math/Matrix.hpp>
+// // Reaktoro includes
+// #include <Reaktoro/Math/Matrix.hpp>
 
-namespace Reaktoro {
+// namespace Reaktoro {
 
-// Forward declarations
-class ChemicalProps;
-class ChemicalState;
-class ChemicalSystem;
-class Partition;
-class EquilibriumProblem;
-struct EquilibriumOptions;
-struct EquilibriumResult;
-struct EquilibriumSensitivity;
+// // Forward declarations
+// class ChemicalProps;
+// class ChemicalState;
+// class ChemicalSystem;
+// class Partition;
+// class EquilibriumProblem;
+// struct EquilibriumOptions;
+// struct EquilibriumResult;
+// struct EquilibriumSensitivity;
 
-/// A solver class for solving chemical equilibrium calculations.
-class EquilibriumSolver
-{
-public:
-    /// Construct an EquilibriumSolver instance
-    explicit EquilibriumSolver(const ChemicalSystem& system);
+// /// A solver class for solving chemical equilibrium calculations.
+// class EquilibriumSolver
+// {
+// public:
+//     /// Construct an EquilibriumSolver instance
+//     explicit EquilibriumSolver(const ChemicalSystem& system);
 
-    /// Construct a copy of an EquilibriumSolver instance
-    EquilibriumSolver(const EquilibriumSolver& other);
+//     /// Construct an EquilibriumSolver instance with given partition
+//     explicit EquilibriumSolver(const Partition& partition);
 
-    /// Destroy this EquilibriumSolver instance
-    virtual ~EquilibriumSolver();
+//     /// Construct a copy of an EquilibriumSolver instance
+//     EquilibriumSolver(const EquilibriumSolver& other);
 
-    /// Assign a copy of an EquilibriumSolver instance
-    auto operator=(EquilibriumSolver other) -> EquilibriumSolver&;
+//     /// Destroy this EquilibriumSolver instance
+//     virtual ~EquilibriumSolver();
 
-    /// Set the options of the equilibrium solver
-    auto setOptions(const EquilibriumOptions& options) -> void;
+//     /// Assign a copy of an EquilibriumSolver instance
+//     auto operator=(EquilibriumSolver other) -> EquilibriumSolver&;
 
-    /// Set the partition of the chemical system
-    auto setPartition(const Partition& partition) -> void;
+//     /// Set the options of the equilibrium solver
+//     auto setOptions(const EquilibriumOptions& options) -> void;
 
-    /// Find an initial feasible guess for an equilibrium problem.
-    /// @param state[in,out] The initial guess and the final state of the equilibrium approximation
-    /// @param be The molar amounts of the elements in the equilibrium partition
-    auto approximate(ChemicalState& state, double T, double P, ArrayXrConstRef be) -> EquilibriumResult;
+//     /// Set the partition of the chemical system
+//     auto setPartition(const Partition& partition) -> void;
 
-    /// Find an initial feasible guess for an equilibrium problem.
-    /// @param state[in,out] The initial guess and the final state of the equilibrium approximation
-    /// @param problem The equilibrium problem with given temperature, pressure, and element amounts.
-    auto approximate(ChemicalState& state, const EquilibriumProblem& problem) -> EquilibriumResult;
+//     /// Find an initial feasible guess for an equilibrium problem.
+//     /// @param state[in,out] The initial guess and the final state of the equilibrium approximation
+//     /// @param be The molar amounts of the elements in the equilibrium partition
+//     auto approximate(ChemicalState& state, double T, double P, ArrayXrConstRef be) -> EquilibriumResult;
 
-    /// Find an initial feasible guess for an equilibrium problem.
-    /// @param state[in,out] The initial guess and the final state of the equilibrium approximation
-    auto approximate(ChemicalState& state) -> EquilibriumResult;
+//     /// Find an initial feasible guess for an equilibrium problem.
+//     /// @param state[in,out] The initial guess and the final state of the equilibrium approximation
+//     /// @param problem The equilibrium problem with given temperature, pressure, and element amounts.
+//     auto approximate(ChemicalState& state, const EquilibriumProblem& problem) -> EquilibriumResult;
 
-    /// Solve an equilibrium problem with given molar amounts of the elements in the equilibrium partition..
-    /// @param state[in,out] The initial guess and the final state of the equilibrium calculation
-    /// @param be The molar amounts of the elements in the equilibrium partition
-    auto solve(ChemicalState& state, double T, double P, ArrayXrConstRef be) -> EquilibriumResult;
+//     /// Find an initial feasible guess for an equilibrium problem.
+//     /// @param state[in,out] The initial guess and the final state of the equilibrium approximation
+//     auto approximate(ChemicalState& state) -> EquilibriumResult;
 
-    /// Solve an equilibrium problem with given molar amounts of the elements in the equilibrium partition..
-    /// @param state[in,out] The initial guess and the final state of the equilibrium calculation
-    /// @param T The temperature (in units of K)
-    /// @param P The pressure (in units of Pa)
-    /// @param be The molar amounts of the elements in the equilibrium partition
-    auto solve(ChemicalState& state, double T, double P, const double* be) -> EquilibriumResult;
+//     /// Solve an equilibrium problem with given molar amounts of the elements in the equilibrium partition..
+//     /// @param state[in,out] The initial guess and the final state of the equilibrium calculation
+//     /// @param be The molar amounts of the elements in the equilibrium partition
+//     auto solve(ChemicalState& state, double T, double P, ArrayXrConstRef be) -> EquilibriumResult;
 
-    /// Solve an equilibrium problem with given equilibrium problem.
-    /// @param state[in,out] The initial guess and the final state of the equilibrium calculation
-    /// @param problem The equilibrium problem with given temperature, pressure, and element amounts.
-    auto solve(ChemicalState& state, const EquilibriumProblem& problem) -> EquilibriumResult;
+//     /// Solve an equilibrium problem with given molar amounts of the elements in the equilibrium partition..
+//     /// @param state[in,out] The initial guess and the final state of the equilibrium calculation
+//     /// @param T The temperature (in units of K)
+//     /// @param P The pressure (in units of Pa)
+//     /// @param be The molar amounts of the elements in the equilibrium partition
+//     auto solve(ChemicalState& state, double T, double P, const double* be) -> EquilibriumResult;
 
-    /// Solve an equilibrium problem with given chemical state in disequilibrium.
-    /// @param state[in,out] The initial guess and the final state of the equilibrium calculation
-    auto solve(ChemicalState& state) -> EquilibriumResult;
+//     /// Solve an equilibrium problem with given equilibrium problem.
+//     /// @param state[in,out] The initial guess and the final state of the equilibrium calculation
+//     /// @param problem The equilibrium problem with given temperature, pressure, and element amounts.
+//     auto solve(ChemicalState& state, const EquilibriumProblem& problem) -> EquilibriumResult;
 
-    /// Return the chemical properties of the calculated equilibrium state.
-    auto props() const -> const ChemicalProps&;
+//     /// Solve an equilibrium problem with given chemical state in disequilibrium.
+//     /// @param state[in,out] The initial guess and the final state of the equilibrium calculation
+//     auto solve(ChemicalState& state) -> EquilibriumResult;
 
-    /// Return the sensitivity of the equilibrium state.
-    /// The sensitivity of the equilibrium state is defined as the rate of change of the
-    /// molar amounts of the equilibrium species with respect to temperature `T`, pressure `P`,
-    /// and molar amounts of equilibrium elements `be`.
-    auto sensitivity() -> const EquilibriumSensitivity&;
+//     /// Return the chemical properties of the calculated equilibrium state.
+//     auto props() const -> const ChemicalProps&;
 
-    /// Compute the sensitivity of the species amounts with respect to temperature.
-    auto dndT() -> ArrayXrConstRef;
+//     /// Return the sensitivity of the equilibrium state.
+//     /// The sensitivity of the equilibrium state is defined as the rate of change of the
+//     /// molar amounts of the equilibrium species with respect to temperature `T`, pressure `P`,
+//     /// and molar amounts of equilibrium elements `be`.
+//     auto sensitivity() -> const EquilibriumSensitivity&;
 
-    /// Compute the sensitivity of the species amounts with respect to pressure.
-    auto dndP() -> ArrayXrConstRef;
+//     /// Compute the sensitivity of the species amounts with respect to temperature.
+//     auto dndT() -> ArrayXrConstRef;
 
-    /// Compute the sensitivity of the species amounts with respect to element amounts.
-    auto dndb() -> ArrayXrConstRef;
+//     /// Compute the sensitivity of the species amounts with respect to pressure.
+//     auto dndP() -> ArrayXrConstRef;
 
-private:
-    struct Impl;
+//     /// Compute the sensitivity of the species amounts with respect to element amounts.
+//     auto dndb() -> ArrayXrConstRef;
 
-    std::unique_ptr<Impl> pimpl;
-};
+// private:
+//     struct Impl;
 
-} // namespace Reaktoro
+//     std::unique_ptr<Impl> pimpl;
+// };
+
+// } // namespace Reaktoro
