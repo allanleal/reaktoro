b55c4a17d23aface4e61a4c7268509d96d91f65a
Allan Leal <allan.leal@erdw.ethz.ch>
2020-05-05 23:52:00 +0200

    Using CMakeRC module to support embedded PHREEQC databases

--- cmake/ReaktoroFindDeps.cmake
+++ cmake/ReaktoroFindDeps.cmake
@@ -1,5 +1,9 @@
-# Find Boost library
-find_package(Boost REQUIRED)
+if(REAKTORO_USE_OPENLIBM)
+    find_package(openlibm REQUIRED)
+endif()
+
+# Find nlohmann_json installed from conda-forge
+find_package(nlohmann_json 3.4.0 REQUIRED)
 
 # Find ThermoFun library and define a pre-processor macro REAKTORO_USING_THERMOFUN if so
 find_package(ThermoFun REQUIRED)
